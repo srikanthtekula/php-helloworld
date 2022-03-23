395ff4152a52457736aa0b39989066a21455c59c
srikanthtekula <79860301+srikanthtekula@users.noreply.github.com>
2022-03-23 15:42:39 +0530

    Create ArrayIndexOutOfBoundException.java
    afdasf

---
+++ ArrayIndexOutOfBoundException.java
@@ -0,0 +1,21 @@
+public class ArrayIndexOutOfBoundException {  
+  
+    public static void main(String[] args) {  
+String[] arr = {"Rohit","Shikar","Virat","Dhoni"};   
+//Declaring 4 elements in the String array                                       
+          
+        for(int i=0;i<=arr.length;i++) {       
+  
+//Here, no element is present at the iteration number arr.length, i.e 4  
+             System.out.println(arr[i]);      
+//So it will throw ArrayIndexOutOfBoundException at iteration 4           
+        }  
+  
+  
+      int num= 25;
+  
+             System.out.println(num/0);      
+  
+    }  
+  
+}
